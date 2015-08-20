b15e1f35c2e8925996699ce777d2eb0a0476ee99
narutards <narutards@hotmail.com>
2015-08-20 14:36:32 +0200

    Update disable-services.ps1

--- scripts/disable-services.ps1
+++ scripts/disable-services.ps1
@@ -3,20 +3,21 @@
 # certain services comment out the corresponding lines below.
 
 $services = @(
-    "HomeGroupListener"
-    "HomeGroupProvider"
-    "MapsBroker"
-    "NetTcpPortSharing"
-    "RemoteAccess"
-    "RemoteRegistry"
-    "SharedAccess"
-    "WbioSrvc"
-    "XblAuthManager"
-    "XblGameSave"
-    "XboxNetApiSvc"
-    "dmwappushservice"
-    "lfsvc"
-    #"wscsvc"
+    "HomeGroupListener" # HomeGroup Listener
+    "HomeGroupProvider" # HomeGroup Provider
+    "MapsBroker" # Downloaded Maps Manager
+    "NetTcpPortSharing" # Net.Tcp Port Sharing Service
+    "RemoteAccess" # Routing and Remote Access
+    "RemoteRegistry" # Remote Registry
+    "SharedAccess" # Internet Connection Sharing (ICS)
+    "WbioSrvc" # Windows Biometric Service
+    #"WlanSvc" # WLAN AutoConfig
+    "XblAuthManager" # Xbox Live Auth Manager
+    "XblGameSave" # Xbox Live Game Save Service
+    "XboxNetApiSvc" # Xbox Live Networking Service
+    "dmwappushservice" # WAP Push Message Routing Service
+    "lfsvc" # Geolocation Service  
+    #"wscsvc" # Windows Security Center Service
 )
 
 foreach ($service in $services) {
