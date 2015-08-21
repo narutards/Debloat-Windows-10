7aca6a651d19c77d8b9d4bb71ef3f16d24a09532
narutards <narutards@hotmail.com>
2015-08-21 08:13:49 +0200

    Update disable_searchfeatures.ps1

--- scripts/disable_searchfeatures.ps1
+++ scripts/disable_searchfeatures.ps1
@@ -8,8 +8,3 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search\' -Name DisableWebSearch -Value 1
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search\' -Name AllowSearchToUseLocation -Value 0
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search\' -Name ConnectedSearchUseWeb -Value 0
-
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Windows Search\' -Name AllowCortana -Value 0
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Windows Search\' -Name DisableWebSearch -Value 1
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Windows Search\' -Name AllowSearchToUseLocation -Value 0
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Windows Search\' -Name ConnectedSearchUseWeb -Value 0
